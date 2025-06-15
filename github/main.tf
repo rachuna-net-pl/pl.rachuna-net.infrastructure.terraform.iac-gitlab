6d3635cb7946e663ea13ec032ebff2e189bc7592
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-06-15 10:02:23 +0200

    params: DEVOPS-15 Definicja i import grupy pl.rachuna-net

---
+++ github/main.tf
@@ -0,0 +1,7 @@
+resource "github_repository" "pl-rachuna-net-infrastructure-terraform-iac-gitlab" {
+  name        = "pl.rachuna-net.infrastructure.terraform.iac-gitlab"
+  description = "Repozytorium zawierające infrastrukturę jako kod (IaC) do zarządzania środowiskiem GitLab przy użyciu Terraform. Umożliwia automatyzację tworzenia, konfiguracji i utrzymania zasobów GitLab."
+
+  visibility = "public"
+
+}
---
+++ main.tf
@@ -0,0 +1,26 @@
+module "group_pl_rachuna-net" {
+  source = "git@gitlab.com:pl.rachuna-net/infrastructure/terraform/modules/gitlab-group.git?ref=feat/2"
+
+  name           = "pl.rachuna-net"
+  description    = "https://rachuna-net.pl"
+  parent_group   = "" # Brak grupy nadrzędnej
+  visibility     = "public"
+  default_branch = "main"
+  labels         = local.labels
+  icon_type      = "root"
+
+  badges    = jsondecode(file("${path.module}/group_badges.json"))
+  variables = jsondecode(file("${path.module}/group_variables.json"))
+}
+
+# module "pl_rachuna-net" {
+#   source = "./pl.rachuna-net/"
+# }
+
+module "github" {
+  source = "./github/"
+
+  github_token = var.github_token
+  github_owner = var.github_owner
+}
+
