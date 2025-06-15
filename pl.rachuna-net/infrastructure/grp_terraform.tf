2114a3cf8340b5f5be695a4a339e7c5853dd1c93
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-06-15 11:19:28 +0200

    params: DEVOPS-17 Definicja i import grupy pl.rachuna-net/infrastructure/terraform

--- pl.rachuna-net/infrastructure/grp_terraform.tf
+++ pl.rachuna-net/infrastructure/grp_terraform.tf
@@ -1,13 +1,13 @@
-module "group_infrastructure" {
+module "group_terraform" {
   source = "git@gitlab.com:pl.rachuna-net/infrastructure/terraform/modules/gitlab-group.git?ref=feat/2"
 
-  name         = "infrastructure"
+  name         = "terraform"
   description  = "Repozytoria i moduły do zarządzania infrastrukturą jako kodem (IaC)."
   parent_group = local.parent_name
   visibility   = "public"
-  icon_type    = "infrastructure"
+  icon_type    = "terraform"
 }
 
-module "infrastructure" {
-  source = "./infrastructure/"
-}
+# module "terraform" {
+#   source = "./terraform/"
+# }
