de44465a53d013a427afe183d4ef90923aea8346
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-06-17 23:29:59 +0200

    params: DEVOPS-37 Definicja repozytorium pl.rachuna-net/cicd/components/validate

--- github/main.tf
+++ github/main.tf
@@ -1,3 +1,21 @@
+resource "github_repository" "pl-rachuna-net-cicd-components-versioning" {
+  name        = "pl.rachuna-net.cicd.components.versioning"
+  description = "Komponent wspierający automatyczne wersjonowanie kodu oraz procesów CI/CD w projektach GitLab."
+  visibility  = "public"
+}
+
+resource "github_repository" "pl-rachuna-net-containers-semantic-release" {
+  name        = "pl.rachuna-net.containers.semantic-release"
+  description = "Obraz Dockerowy z narzędziem Semantic-Release."
+  visibility  = "public"
+}
+
+resource "github_repository" "pl-rachuna-net-containers-terraform" {
+  name        = "pl.rachuna-net.containers.terraform"
+  description = "Obraz Dockerowy z narzędziem Terraform."
+  visibility  = "public"
+}
+
 resource "github_repository" "pl-rachuna-net-infrastructure-terraform-iac-gitlab" {
   name        = "pl.rachuna-net.infrastructure.terraform.iac-gitlab"
   description = "Repozytorium zawierające infrastrukturę jako kod (IaC) do zarządzania środowiskiem GitLab przy użyciu Terraform. Umożliwia automatyzację tworzenia, konfiguracji i utrzymania zasobów GitLab."
@@ -13,17 +31,5 @@ resource "github_repository" "pl-rachuna-net-infrastructure-terraform-modules-gi
 resource "github_repository" "pl-rachuna-net-infrastructure-terraform-modules-gitlab-project" {
   name        = "pl.rachuna-net.infrastructure.terraform.modules.gitlab-project"
   description = "Moduł służy do automatycznego zakładania projektów w GitLab z wykorzystaniem standaryzacji, ochrony branchy, tagów, zmiennych CI oraz integracji z SonarQube."
-  visibility  = "public"
-}
-
-resource "github_repository" "pl-rachuna-net-containers-terraform" {
-  name        = "pl.rachuna-net.containers.terraform"
-  description = "Obraz Dockerowy z narzędziem Terraform."
-  visibility  = "public"
-}
-
-resource "github_repository" "pl-rachuna-net-containers-semantic-release" {
-  name        = "pl.rachuna-net.containers.semantic-release"
-  description = "Obraz Dockerowy z narzędziem Semantic-Release."
   visibility  = "public"
 }
