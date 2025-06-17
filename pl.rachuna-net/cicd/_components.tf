976e9de8f960a3b68081728f204d84483cdd18ce
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-06-17 22:55:29 +0200

    params: DEVOPS-43 Przygotowanie repozytorium pl.rachuna-net/containers/semantic-release

--- pl.rachuna-net/cicd/_components.tf
+++ pl.rachuna-net/cicd/_components.tf
@@ -2,7 +2,7 @@ module "_components" {
   source = "git@gitlab.com:pl.rachuna-net/infrastructure/terraform/modules/gitlab-group.git?ref=v1.0.0"
 
   name         = "components"
-  description = "Wspólne komponenty wykorzystywane w procesach CI/CD."
+  description  = "Wspólne komponenty wykorzystywane w procesach CI/CD."
   parent_group = local.parent_name
   visibility   = "public"
   icon_type    = "gitlab"
---
+++ pl.rachuna-net/containers/semantic-release.tf
@@ -0,0 +1,16 @@
+module "semantic-release" {
+  source = "git@gitlab.com:pl.rachuna-net/infrastructure/terraform/modules/gitlab-project.git?ref=v1.0.0"
+
+  name        = "semantic-release"
+  description = "Obraz Dockerowy z narzędziem Semantic-Release."
+  visibility  = "public"
+  tags        = ["docker", "semantic-release"]
+  icon_type   = "semantic-release"
+
+  parent_group = local.parent_name
+  project_type = local.project_type
+
+  # sonarqube
+  sonarqube_cloud_project_id = "pl.rachuna-net_semantic-release"
+  is_enabled_sonarqube       = true
+}
