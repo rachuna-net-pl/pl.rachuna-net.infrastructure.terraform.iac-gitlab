0640b2bc24c8d36d12378774ce890ec3346fd517
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-06-22 10:04:18 +0200

    params: DEVOPS-116 Utworzenie repozytorium dla projektu pl.rachuna-net/infrastructure/packer/alpine

--- github/ pl.rachuna-net.infrastructure.packer.tf
+++ github/ pl.rachuna-net.infrastructure.packer.tf
@@ -3,3 +3,9 @@ resource "github_repository" "pl-rachuna-net-infrastructure-packer-ubuntu" {
   description = ""
   visibility  = "public"
 }
+
+resource "github_repository" "pl-rachuna-net-infrastructure-packer-alpine" {
+  name        = " pl.rachuna-net.infrastructure.packer.alpine"
+  description = ""
+  visibility  = "public"
+}
