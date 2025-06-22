42fdd84907da19cb1dbc4f886c1375c88b0cd5c8
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-06-22 09:29:30 +0200

    params: DEVOPS-85 Utworzenie repozytorium pl.rachuna-net/infrastructure/terraform/iac-vault

---
+++ pl.rachuna-net/infrastructure/terraform/iac-vault.tf
@@ -0,0 +1,16 @@
+module "iac-vault" {
+  source = "git@gitlab.com:pl.rachuna-net/infrastructure/terraform/modules/gitlab-project.git?ref=v1.0.1"
+
+  name        = "iac-vault"
+  description = "Repozytorium zawierające infrastrukturę jako kod (IaC) do zarządzania środowiskiem Vault przy użyciu Terraform. Umożliwia automatyzację tworzenia, konfiguracji i utrzymania zasobów Vault."
+  visibility  = "public"
+  tags        = ["terraform"]
+  icon_type   = "vault"
+
+  parent_group = local.parent_name
+  project_type = local.project_type
+
+  # sonarqube
+  sonarqube_cloud_project_id = "pl.rachuna-net_iac-vault"
+  is_enabled_sonarqube       = true
+}
