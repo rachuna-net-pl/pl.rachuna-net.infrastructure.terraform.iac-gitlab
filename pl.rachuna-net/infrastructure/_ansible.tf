a623dc3eb560ab49a14b8595ae557dd8aa8ecdc9
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-06-23 07:45:23 +0200

    params: DEVOPS-138 Utworzenie grupy repozytoriów pl.rachuna-net/infrastructure/ansible

---
+++ pl.rachuna-net/infrastructure/_ansible.tf
@@ -0,0 +1,13 @@
+module "_ansible" {
+  source = "git@gitlab.com:pl.rachuna-net/infrastructure/terraform/modules/gitlab-group.git?ref=v1.1.0"
+
+  name         = "ansible"
+  description  = "Repozytoria i moduły do zarządzania konfiguracją i automatyzacją zadań przy użyciu Ansible."
+  parent_group = local.parent_name
+  visibility   = "public"
+  icon_type    = "ansible"
+}
+
+# module "ansible" {
+#   source = "./ansible/"
+# }
